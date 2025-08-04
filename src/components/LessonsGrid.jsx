172c3afe31a73b83e8fe2b0c9ab8428a2fab5302
albal <albaluccia@gmail.com>
2025-08-04 17:59:04 +0800

    created structure

---
+++ src/components/LessonsGrid.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+export const lessonsGrid = () => {
+    return(
+        <div></div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+export const Home = () => {
+    return(
+        <div>
+            <h1>Learn something new every day</h1>
+            <p>
+                Discovery daily bite-sized lessons in various topics, 
+                designed to expand your knowledge
+            </p>
+
+        </div>
+    )
+}
