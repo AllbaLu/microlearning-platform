5f3e83faac735400bd6b842766911564ed0f0692
albal <albaluccia@gmail.com>
2025-08-06 13:36:51 +0800

    created routes/chat Groq AI

---
+++ src/backend/routes/chat.js
@@ -0,0 +1,40 @@
+
+
+const express = require('express');
+const axios = require('axios');
+require('donenv').config();
+
+const router = express.Router();
+
+
+router.post('/api/chat', async (req, res) => {
+    const { prompt } = req.body;
+
+
+    try {
+        const response = await axios.post(
+            'https://api.groq.com/openai/v1/chat/completions',
+        {
+          model: 'llama3-70b-8192',
+          messages: [
+            { role: 'system', content: 'you are an expert in microlearning.'},
+            { role: 'user', content: prompt},
+          ],
+          temperature:0.7,
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
+            },
+        }
+    );
+
+    res,express.json(response.data);
+    } catch (error) {
+        console.log('Error in Groq:', error,message);
+        res.status(500).json({error:'Error response Groq'}); 
+    }
+});
+
+module.exports = router;
